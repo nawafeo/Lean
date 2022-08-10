7d679b1de0ef8c97ce5eb89595e98308646478bc
Nawaf Osman (Laptop) <nawaf3@cs.washington.edu>
2022-08-10 17:17:45 -0500

    Initial Commit

---
+++ src/main/java/com/github/nawafeo/Workout_and_Diet_Planner/WorkoutAndDietPlannerApplication.java
@@ -0,0 +1,13 @@
+package com.github.nawafeo.Workout_and_Diet_Planner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkoutAndDietPlannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkoutAndDietPlannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/nawafeo/Workout_and_Diet_Planner/WorkoutAndDietPlannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.nawafeo.Workout_and_Diet_Planner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutAndDietPlannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
